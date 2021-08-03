c74f54ea32e31942c8de3db88731e96c935db1fd
Liam <liam@scalzulli.com>
2021-08-03 19:14:27 -0400

    [nvim] configure autopairs cursor placement

--- config/nvim/lua/config.lua
+++ config/nvim/lua/config.lua
@@ -184,3 +184,17 @@ g.vim_markdown_conceal_code_blocks = 0
 -- ╚────────────────────────────────────────────────────────────────────────────│──
 
 require('nvim-autopairs').setup()
+
+local npairs = require('nvim-autopairs')
+
+_G.MUtils = {}
+
+MUtils.completion_confirm = function()
+  if vim.fn.pumvisible() ~= 0 then
+    return npairs.esc("<cr>")
+  else
+    return npairs.autopairs_cr()
+  end
+end
+
+map('i', '<CR>', 'v:lua.MUtils.completion_confirm()', { expr = true , noremap = true })
