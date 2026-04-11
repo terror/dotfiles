988b8cfee2c67ad93c8a868ecd7158e1b6f5910d
Liam <liam@scalzulli.com>
2026-04-10 22:37:42 -0400

    Disable legacy neovim plugin providers

--- etc/nvim/lua/config.lua
+++ etc/nvim/lua/config.lua
@@ -3,6 +3,11 @@ require('util')
 
 local g = vim.g
 
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+
 -- ───────────────────────────────────────────────────────────────────────────-─╗
 -- │ Telescope                                                                  │
 -- ╚────────────────────────────────────────────────────────────────────────────│
